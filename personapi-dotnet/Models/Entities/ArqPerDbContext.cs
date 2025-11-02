c8d578917044d0d7fefa87e1b0e219ea8ae7f8bf
CryoWarrior <diegoalejandrojararojas@gmail.com>
2025-11-02 18:16:01 -0500

    Creacion proyecto y Docker

---
+++ personapi-dotnet/Models/Entities/ArqPerDbContext.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace personapi_dotnet.Models.Entities;
+
+public partial class ArqPerDbContext : DbContext
+{
+    public ArqPerDbContext()
+    {
+    }
+
+    public ArqPerDbContext(DbContextOptions<ArqPerDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Estudio> Estudios { get; set; }
+
+    public virtual DbSet<Persona> Personas { get; set; }
+
+    public virtual DbSet<Profesion> Profesions { get; set; }
+
+    public virtual DbSet<Telefono> Telefonos { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=localhost,1433;Database=arq_per_db;User Id=sa;Password=YourStrong!Passw0rd;TrustServerCertificate=true;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Estudio>(entity =>
+        {
+            entity.HasKey(e => new { e.IdProf, e.CcPer });
+
+            entity.ToTable("estudios");
+
+            entity.Property(e => e.IdProf).HasColumnName("id_prof");
+            entity.Property(e => e.CcPer).HasColumnName("cc_per");
+            entity.Property(e => e.Fecha).HasColumnName("fecha");
+            entity.Property(e => e.Univer)
+                .HasMaxLength(50)
+                .IsUnicode(false)
+                .HasColumnName("univer");
+
+            entity.HasOne(d => d.CcPerNavigation).WithMany(p => p.Estudios)
+                .HasForeignKey(d => d.CcPer)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_estudio_persona");
+
+            entity.HasOne(d => d.IdProfNavigation).WithMany(p => p.Estudios)
+                .HasForeignKey(d => d.IdProf)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_estudio_profesion");
+        });
+
+        modelBuilder.Entity<Persona>(entity =>
+        {
+            entity.HasKey(e => e.Cc).HasName("PK__persona__3213666D63538D42");
+
+            entity.ToTable("persona");
+
+            entity.Property(e => e.Cc)
+                .ValueGeneratedNever()
+                .HasColumnName("cc");
+            entity.Property(e => e.Apellido)
+                .HasMaxLength(45)
+                .IsUnicode(false)
+                .HasColumnName("apellido");
+            entity.Property(e => e.Edad).HasColumnName("edad");
+            entity.Property(e => e.Genero)
+                .HasMaxLength(1)
+                .IsUnicode(false)
+                .IsFixedLength()
+                .HasColumnName("genero");
+            entity.Property(e => e.Nombre)
+                .HasMaxLength(45)
+                .IsUnicode(false)
+                .HasColumnName("nombre");
+        });
+
+        modelBuilder.Entity<Profesion>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__profesio__3213E83FC5D5ACDA");
+
+            entity.ToTable("profesion");
+
+            entity.Property(e => e.Id)
+                .ValueGeneratedNever()
+                .HasColumnName("id");
+            entity.Property(e => e.Des)
+                .IsUnicode(false)
+                .HasColumnName("des");
+            entity.Property(e => e.Nom)
+                .HasMaxLength(90)
+                .IsUnicode(false)
+                .HasColumnName("nom");
+        });
+
+        modelBuilder.Entity<Telefono>(entity =>
+        {
+            entity.HasKey(e => e.Num).HasName("PK__telefono__DF908D6597590DEC");
+
+            entity.ToTable("telefono");
+
+            entity.Property(e => e.Num)
+                .HasMaxLength(15)
+                .IsUnicode(false)
+                .HasColumnName("num");
+            entity.Property(e => e.Duenio).HasColumnName("duenio");
+            entity.Property(e => e.Oper)
+                .HasMaxLength(45)
+                .IsUnicode(false)
+                .HasColumnName("oper");
+
+            entity.HasOne(d => d.DuenioNavigation).WithMany(p => p.Telefonos)
+                .HasForeignKey(d => d.Duenio)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_telefono_persona");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ personapi-dotnet/init-full.bat
@@ -0,0 +1,51 @@
+﻿@echo off
+setlocal
+
+echo ==========================================
+echo 1/3 - Levantando contenedores Docker
+echo ==========================================
+docker compose up -d
+
+if %errorlevel% neq 0 (
+    echo  Error al levantar los contenedores.
+    exit /b %errorlevel%
+)
+
+echo ==========================================
+echo 🧩 2/3 - Ejecutando init.sql dentro del contenedor SQL Server
+echo ==========================================
+docker exec -it sqlserver /opt/mssql-tools18/bin/sqlcmd ^
+  -S localhost -U sa -P "YourStrong!Passw0rd" -C ^
+  -i /docker-entrypoint-initdb.d/init.sql
+
+if %errorlevel% neq 0 (
+    echo Error al ejecutar el script SQL.
+    exit /b %errorlevel%
+)
+
+echo ==========================================
+echo 🧠 3/3 - Generando entidades con Scaffold-DbContext
+echo ==========================================
+
+:: Mueve a la carpeta del proyecto (ajusta si tu .csproj está en subcarpeta)
+cd /d "%~dp0"
+
+dotnet ef dbcontext scaffold ^
+  "Server=localhost,1433;Database=arq_per_db;User Id=sa;Password=YourStrong!Passw0rd;TrustServerCertificate=true;" ^
+  Microsoft.EntityFrameworkCore.SqlServer ^
+  --output-dir Models/Entities --context ArqPerDbContext --force
+
+if %errorlevel% neq 0 (
+    echo Error en Scaffold-DbContext
+    exit /b %errorlevel%
+)
+
+echo ==========================================
+echo Base creada y entidades generadas correctamente
+echo ==========================================
+
+docker exec -it sqlserver /opt/mssql-tools18/bin/sqlcmd ^
+  -S localhost -U sa -P "YourStrong!Passw0rd" -C ^
+  -Q "SELECT name FROM sys.databases"
+
+pause
