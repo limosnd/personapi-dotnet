9d69e8a3c791c2f37779ae30dad11571f1c906dd
CryoWarrior <diegoalejandrojararojas@gmail.com>
2025-11-03 20:13:53 -0500

    Arreglo localhost

--- personapi-dotnet/obj/Debug/net8.0/personapi-dotnet.AssemblyInfo.cs
+++ personapi-dotnet/obj/Debug/net8.0/personapi-dotnet.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("personapi-dotnet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7c818590919647fe9b6505c26b796d99c516446")]
 [assembly: System.Reflection.AssemblyProductAttribute("personapi-dotnet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("personapi-dotnet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
